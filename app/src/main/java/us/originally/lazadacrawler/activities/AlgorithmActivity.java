85d38eb692fbd2d6efdc4479ba062ad3c00342ec
Tuan_Anh <anhnt.2309@gmail.com>
2017-12-05 15:55:57 +0700

    new activity to run and display apriori algorithm

---
+++ app/src/main/java/us/originally/lazadacrawler/activities/AlgorithmActivity.java
@@ -0,0 +1,144 @@
+package us.originally.lazadacrawler.activities;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+import us.originally.lazadacrawler.CrawlerDB;
+import us.originally.lazadacrawler.MainActivity;
+import us.originally.lazadacrawler.R;
+import us.originally.lazadacrawler.models.Product;
+import weka.associations.Apriori;
+import weka.associations.AssociationRule;
+import weka.associations.AssociatorEvaluation;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.FastVector;
+import weka.core.Instances;
+import weka.filters.Filter;
+
+/**
+ * Created by TuanAnh on 12/4/17.
+ */
+
+public class AlgorithmActivity extends AppCompatActivity {
+    private ArrayList<String> detailUrls;
+    private Instances instances;
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_algorithm);
+        tvResult = findViewById(R.id.tv_algorithm_result);
+
+        detailUrls = MainActivity.getCrawledLinkFromDb(this);
+        ArrayList<Product> products = new ArrayList<>();
+        for (String model : detailUrls) {
+            Product product = new Gson().fromJson(model, Product.class);
+            products.add(product);
+        }
+
+        Log.e("x", "" + products.size());
+
+        //construct weka Instances object with crawled data
+        ArrayList<Attribute> atts = new ArrayList<Attribute>(8);
+        ArrayList<String> classVal = new ArrayList<String>();
+        classVal.add("A");
+        classVal.add("B");
+        atts.add(new Attribute("installment", (ArrayList<String>) null));
+        atts.add(new Attribute("current_price", (ArrayList<String>) null));
+        atts.add(new Attribute("percen_sale", (ArrayList<String>) null));
+        atts.add(new Attribute("saler_rate", (ArrayList<String>) null));
+        atts.add(new Attribute("saler_saleTime", (ArrayList<String>) null));
+        atts.add(new Attribute("warranty_time", (ArrayList<String>) null));
+        atts.add(new Attribute("product_rate", (ArrayList<String>) null));
+        atts.add(new Attribute("brand", (ArrayList<String>) null));
+
+
+        instances = new Instances("TestInstances", atts, products.size());
+        System.out.println("Before adding any instance");
+        System.out.println("--------------------------");
+        System.out.println(instances);
+        System.out.println("--------------------------");
+        for (Product product : products) {
+            double[] instanceValue1 = new double[instances.numAttributes()];
+
+            instanceValue1[0] = instances.attribute(0).addStringValue(product.installment);
+            instanceValue1[1] = instances.attribute(1).addStringValue(product.current_price);
+            instanceValue1[2] = instances.attribute(2).addStringValue(product.percen_sale);
+            instanceValue1[3] = instances.attribute(3).addStringValue(product.saler.saler_rate);
+            instanceValue1[4] = instances.attribute(4).addStringValue(product.saler.getSalerSalingTime());
+            instanceValue1[5] = instances.attribute(5).addStringValue(product.getWarranty());
+            instanceValue1[6] = instances.attribute(6).addStringValue(product.productRating.overall_rate);
+            instanceValue1[7] = instances.attribute(7).addStringValue(product.brand);
+
+            instances.add(new DenseInstance(1.0, instanceValue1));
+        }
+
+        System.out.println("After adding any instance");
+        System.out.println("--------------------------");
+        System.out.println(instances);
+        System.out.println("--------------------------");
+
+
+        //Apply filter to convert String to Nominal to run Apriori
+        String[] options = new String[2];
+        options[0] = "-R";                // "range"
+//            options[1] = "4"; // first attribute
+
+        weka.filters.unsupervised.attribute.StringToNominal ff = new weka.filters.unsupervised.attribute.StringToNominal(); // new instance of filter
+
+        try {
+//                ff.setOptions(options);
+            ff.setAttributeRange("first-last");
+            ff.setInputFormat(instances);
+            instances = Filter.useFilter(instances, ff);// set options
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        //run weka apriori api
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //get data from url
+//                        String dataSet = "http://storm.cis.fordham.edu/~gweiss/data-mining/weka-data/weather.nominal.arff";
+//                        ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource(dataSet);
+//                        Instances instances = dataSource.getDataSet();
+
+                    //local data
+
+                    final Apriori model = new Apriori();
+                    model.setNumRules(1000);
+                    model.buildAssociations(instances);
+                    Log.d("xxx", model.toString());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvResult.setText(model.toString());
+                        }
+                    });
+
+//                    for (AssociationRule associationRule : model.getAssociationRules().getRules()) {
+//                        Log.e("Rules", associationRule.getPremise().toString());
+//                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+
+}
