035951d6cc9be7780d07f369b2b585481a35a2d7
Tuan_Anh <anhnt.2309@gmail.com>
2017-12-10 23:16:25 +0700

    update models

--- app/src/main/java/us/originally/lazadacrawler/models/Product.java
+++ app/src/main/java/us/originally/lazadacrawler/models/Product.java
@@ -7,8 +7,10 @@ import java.util.ArrayList;
  */
 
 public class Product {
+    public String product_url;
     public String name;
     public String brand;
+    public String category;
     public String brandUrl;
     public ArrayList<String> detail;
     public ArrayList<String> imageUrls;
@@ -38,13 +40,15 @@ public class Product {
     public Product() {
     }
 
-    public Product(String name, String brand, String brandUrl, ArrayList<String> detail, ArrayList<String> imageUrls,
+    public Product(String product_url,String name, String brand,String category, String brandUrl, ArrayList<String> detail, ArrayList<String> imageUrls,
                    String current_price, String old_price, String currency, String percen_sale, String installment,
                    String warranty_time, String warranty_type, String warranty_detail, String pay_method, String payback_policy,
                    String payback_subtitle, String payback_detail, String product_included, Saler saler, ProductRating productRating,
                    ArrayList<Commnent> commnent, String saling_location) {
+        this.product_url = product_url;
         this.name = name;
         this.brand = brand;
+        this.category = category;
         this.brandUrl = brandUrl;
         this.detail = detail;
         this.imageUrls = imageUrls;
@@ -53,6 +57,8 @@ public class Product {
         this.old_price = old_price;
         this.currency = currency;
         this.percen_sale = percen_sale;
+        if(percen_sale.isEmpty())
+            this.percen_sale = "0%";
         this.installment = installment;
 
         this.warranty_time = warranty_time;
---
+++ app/src/main/java/us/originally/lazadacrawler/models/js/AssetScript.java
@@ -0,0 +1,94 @@
+package us.originally.lazadacrawler.models.js;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.efurture.script.JSTransformer;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by TuanAnh on 12/10/17.
+ */
+
+public class AssetScript {
+
+    private static Map<String,String> scripts =new HashMap<String,String>();
+
+    public static String read(Context context, String fileName){
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+            BufferedInputStream bufferIn = new BufferedInputStream(is);
+            byte[] buffer = new byte[2048];
+            int length = 0;
+            while ((length = bufferIn.read(buffer)) >= 0){
+                output.write(buffer, 0, length);
+            }
+            String script = output.toString();
+            is.close();
+            Log.e("compiled ScriptEngine", script);
+            return script;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String toScript(Context context, String fileName){
+        try {
+            String script = scripts.get(fileName);
+            if(!TextUtils.isEmpty(script)){
+                return  script;
+            }
+            Log.e("DuktapeJava", "Read" );
+            InputStream is = context.getAssets().open(fileName);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            Log.e("DuktapeJava", "Transform" );
+            script = JSTransformer.parse(br);
+            is.close();
+            scripts.put(fileName, script);
+            Log.e("ScriptEngine", script);
+            return script;
+        } catch (Exception e) {
+            Log.e("ScriptEngine", e.toString());
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+    public static String toScript(String fileName){
+        try {
+            InputStream is = new FileInputStream(fileName);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String script =  JSTransformer.parse(br);
+            is.close();
+            Log.e("ScriptEngine", script);
+            return script;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static String toScript(InputStream is){
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String script =  JSTransformer.parse(br);
+            Log.e("ScriptEngine", script);
+            return script;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
